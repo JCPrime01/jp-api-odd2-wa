aedb3d40639505c29728dbd48629f527037875f9
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-19 21:49:38 -0300

    chore: update shareCode to XVXFAWE60HG

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/63ycdffd?shareCode=I51GTG21WEM&afp=tapi2' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/63ycdffd?shareCode=XVXFAWE60HG&afp=tapi2' });
     res.end();
   }
