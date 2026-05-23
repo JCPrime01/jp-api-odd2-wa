fbf9954ba2560e2fcd8dd179fcfd038be44bc4de
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-22 22:00:40 -0300

    chore: update shareCode to KXZLO0DSTZ7

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/63ycdffd?shareCode=TIBM0HY0ZJN&afp=tapi2' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/63ycdffd?shareCode=KXZLO0DSTZ7&afp=tapi2' });
     res.end();
   }
