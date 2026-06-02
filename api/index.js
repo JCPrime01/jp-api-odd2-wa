b84ab2fde8733aa09d4474f48e1225ab43d6f3e7
JCPrime01 <vanzokiller@gmail.com>
2026-06-01 22:38:43 -0300

    chore: shareCode ZSOBJP7PKYM -> 1AQT336XWO3 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/63ycdffd?shareCode=ZSOBJP7PKYM&afp=ta' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/63ycdffd?shareCode=1AQT336XWO3&afp=ta' });
     res.end();
   }
