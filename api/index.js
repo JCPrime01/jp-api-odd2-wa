d0ec23bc2cffe16b9fc3f2b9a4fe3d867e423505
JCPrime01 <vanzokiller@gmail.com>
2026-06-15 20:13:26 -0300

    chore: shareCode HS4MNEMIYGQ -> UQA7QLMZM9Q (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
     
     const url = 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180'
 
-    const shareCode = '&shareCode=HS4MNEMIYGQ'
+    const shareCode = '&shareCode=UQA7QLMZM9Q'
     
     const afp = '&afp2=ta'
     
