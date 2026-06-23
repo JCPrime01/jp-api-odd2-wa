05ea2aa43ddd093e4192780116bbab89e43e37ff
JCPrime01 <vanzokiller@gmail.com>
2026-06-23 20:24:34 -0300

    chore: shareCode JV9UMH3JXNM -> DXZ76MO53KC (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
     
     const url = 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180'
 
-    const shareCode = '&shareCode=JV9UMH3JXNM'
+    const shareCode = '&shareCode=DXZ76MO53KC'
     
     const afp = '&afp2=ta'
     
