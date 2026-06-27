2a2fa832fb2e0dda6a54c732e8803e686f10ad60
JCPrime01 <vanzokiller@gmail.com>
2026-06-26 21:46:13 -0300

    chore: shareCode ZM02W2CK4TI -> QDO5PWRFWHL (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
     
     const url = 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180'
 
-    const shareCode = '&shareCode=ZM02W2CK4TI'
+    const shareCode = '&shareCode=QDO5PWRFWHL'
     
     const afp = '&afp2=ta'
     
