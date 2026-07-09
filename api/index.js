9c42e18276a6e508c328f17f071291ee001a2342
JCPrime01 <vanzokiller@gmail.com>
2026-07-08 21:49:02 -0300

    chore: shareCode GUOP5B88W64 -> W90BB43HPA5 (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
     
     const url = 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180'
 
-    const shareCode = '&shareCode=GUOP5B88W64'
+    const shareCode = '&shareCode=W90BB43HPA5'
     
     const afp = '&afp2=ta'
     
