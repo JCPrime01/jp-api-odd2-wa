af1da87828bc368deaf93c853a4c5581a38ca12f
JCPrime01 <vanzokiller@gmail.com>
2026-07-13 22:36:54 -0300

    chore: shareCode X78L2958O2F -> P2IEKUVI27I (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
     
     const url = 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180'
 
-    const shareCode = '&shareCode=X78L2958O2F'
+    const shareCode = '&shareCode=P2IEKUVI27I'
     
     const afp = '&afp2=ta'
     
