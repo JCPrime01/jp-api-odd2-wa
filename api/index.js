70015dfb8bb746aa2adad94e43ec64d3a609933b
JCPrime01 <vanzokiller@gmail.com>
2026-07-20 23:26:32 -0300

    chore: shareCode ALI0VV4G5VR -> HVZKJURTNET (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
     
     const url = 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180'
 
-    const shareCode = '&shareCode=ALI0VV4G5VR'
+    const shareCode = '&shareCode=HVZKJURTNET'
     
     const afp = '&afp2=ta'
     
